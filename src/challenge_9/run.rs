ea283cfcbf77bdbfd93d6a665795c937ec9507c5
TaianRmos <henri.dujarrier@gmail.com>
2025-12-09 15:16:49 +0100

    Making great progress of part 2

--- src/challenge_9/run.rs
+++ src/challenge_9/run.rs
@@ -41,6 +41,7 @@ pub fn part_1() -> std::io::Result<()> {
 
 
 pub fn part_2() -> std::io::Result<()> {
+    // Reading the file and extracting the points
     let content: String = fs::read_to_string("src/challenge_9/test.txt")?;
     let points: Vec<(i64, i64)> = content.lines()
         .map(|s| {
@@ -56,112 +57,37 @@ pub fn part_2() -> std::io::Result<()> {
         .fold((i64::MIN, i64::MIN), |(mx, my), (x, y)| {
             (mx.max(*x), my.max(*y))
         });
-        
-    let mut grid: Vec<Vec<char>> = vec![vec!['.'; (max_x + 3) as usize]; (max_y + 3) as usize];
-    for point in &points {
-        grid[point.1 as usize][point.0 as usize] = '#';
-    }
-    print_grid(&grid);
-    println!();
-
-    // Complete the shape horizontally
-    let mut is_filling: bool = false;
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            // If it's a #, we start filling if we weren't
-            // and we stop if it's the second #
-            if grid[i][j] == '#' {
-                if is_filling {
-                    is_filling = false;
-                }
-                else {
-                    is_filling = true;
-                }
-            }
-            // Fill the grid if needed
-            else if is_filling {
-                grid[i][j] = 'X';
-            }
-        }
-    }
-
-
-    // Complete the shape vertically
-    is_filling = false;
-    for j in 0..grid[0].len() {
-        for i in 0..grid.len() {
-            // If it's a #, we start filling if we weren't
-            // and we stop if it's the second #
-            if grid[i][j] == '#' {
-                if is_filling {
-                    is_filling = false;
-                }
-                else {
-                    is_filling = true;
-                }
-            }
-            // Fill the grid if needed
-            else if is_filling {
-                grid[i][j] = 'X';
-            }
-        }
-    }
-
-    // Change path by #
-    for row in &mut grid {
-        for cell in row {
-            if *cell == 'X' {
-                *cell = '#';
-            }
-        }
-    }
-
-    print_grid(&grid);
-    println!();
-
-    // Fill the grid
-    let mut is_filling: bool = false;
-    for i in 0..grid.len() {
-        let mut nb_hashtags: usize = grid[i].iter().filter(|&&c| c == '#').count();
-
-        for j in 0..grid[0].len() {
-            // If it's a #, we start filling if we weren't
-            // and we stop if it's the second #
-            if grid[i][j] == '#' {
-                nb_hashtags -= 1;
-                if nb_hashtags > 0 {
-                    is_filling = true;
-                }
-                else {
-                    is_filling = false;
-                }
-            }
-            // Fill the grid if needed
-            else if is_filling {
-                grid[i][j] = '#';
-            }
-        }
-    }
     
+    // points_on_row[x] has the list of y coordinates where we have points on row x
+    // Since we should have only 2 (verified), it's either [0, 0], or [ya, yb]
+    let mut points_on_row: Vec<Vec<i64>> = vec![Vec::new(); (max_y + 1) as usize];
+    let mut points_on_column: Vec<Vec<i64>> = vec![Vec::new(); (max_x + 1) as usize];
 
-    print_grid(&grid);
+    // Fill the points into the rows and columns
+    for point in &points {
+        let (x, y) = point.clone();
+        points_on_row[y as usize].push(x);
+        points_on_column[x as usize].push(y);
+    }
 
+    // Fill the empty vectors with 2 zeros, and sort to simplify the use case
+    for i in 0..points_on_row.len() {
+        if points_on_row[i].len() == 0 {
+            points_on_row[i].push(0);
+            points_on_row[i].push(0);
+        }
+        else {
+            points_on_row[i].sort();
+        }
+    }
 
-    let mut max_area: i64 = 0;
-    for i in 0..points.len()-1 {
-        let point_a: (i64, i64) = points[i];
-        for j in i+1..points.len() {
-            let point_b: (i64, i64) = points[j];
-
-            if grid[point_b.1 as usize][point_a.0 as usize] == '.' || grid[point_a.1 as usize][point_b.0 as usize] == '.' {
-                continue;
-            }    
-
-            let area: i64 = ((point_a.0 - point_b.0).abs() + 1) * ((point_a.1 - point_b.1).abs() + 1);
-
-            if area > max_area {
-                max_area = area;
-            }
+    for i in 0..points_on_column.len() {
+        if points_on_column[i].len() == 0 {
+            points_on_column[i].push(0);
+            points_on_column[i].push(0);
+        }
+        else {
+            points_on_column[i].sort();
         }
     }
 
